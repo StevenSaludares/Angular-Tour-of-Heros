764576d95ffa0b255399039248f529fa581eacb9
StevenSaludares <steveblank14@gmail.com>
2017-12-08 07:45:37 +0800

    add hero.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+hero = 'Windstorm';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
-  hero = 'Windstorm';
+ 
 })
 export class HeroesComponent implements OnInit {
 
   constructor() { }
-
+ 
   ngOnInit() {
   }
 
